49ad3db473e29c736d29c235d0e18562a79e7976
huanghaorui <huanghaorui97@163.com>
2019-09-18 18:31:08 +0800

    first

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./router/index";
 import store from "./store";
 
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "@/assets/scss/index.scss";
+
+import "@/icons";
+
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
